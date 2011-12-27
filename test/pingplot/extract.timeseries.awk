41f85186da84279bb0245bef55852bc12299f2a0
Dave Taht <d@taht.net>
2011-12-27 14:17:47 +0100

    Added Ping plot scripts
    Fabian wrote some scripts in awk and R to extract
    and plot pings from an experiment I did in class showing bufferbloat
    in action and the effects of a reduced txqueue length.
    
    While I liked R, and am impressed with fabian's use of awk,
    coming up with a good plotting/graphic/data extraction solution
    remains on my task list. That said, these are useful examples
    of useful output tools.

---
+++ test/pingplot/extract.timeseries.awk
@@ -0,0 +1,42 @@
+BEGIN {
+    ERR = "/dev/stderr"
+    timeout_offset = 100.00
+}
+
+/timeout/ {
+#    rtt[NR] = "max"    
+    next
+}
+
+/DUP/ {
+#    rtt[NR] = "max"    
+    next
+}
+
+{#
+    rtt[NR] = $10  
+    if ($10 > max_rtt) {
+	max_rtt = $10
+	max_rtt_nr = NR
+    }
+}
+
+END {
+    print "max:", max_rtt, "at:", max_rtt_nr > ERR
+    
+    if ( (max_rtt_nr - 100) < 0 )
+	start = 1
+    else
+	start = max_rtt_nr - 100
+
+    if ( (max_rtt_nr + 100) > NR )
+	stop = NR
+    else
+	stop = max_rtt_nr + 100
+
+    for ( i = start; i <= stop; i ++) {
+	if (rtt[i] == "max")
+	    rtt[i] = max_rtt + timeout_offset
+	print rtt[i]
+    }
+}
---
+++ test/pingplot/plot1.R
@@ -0,0 +1,20 @@
+p1 <- read.table("p1.ts.cut")
+p2 <- read.table("p2.ts.cut")
+
+postscript("plot1.ps", width = 6, height =4)
+
+par(mar=c(3,3,2,1), mgp=c(2,0.5,0))
+
+l = max( length(p1[,1]), length(p2[,1]))
+h = max( p1[,1], p2[,1])
+
+plot( p1[,1], ylab = "Wireless ping RTTs", xlab="Time [ping interval 1 sec]", 
+    xlim = c(0,l), ylim = c(0,h), lty = 1, pch = 1, col = 1, type='o')
+
+lines( p2[,1], lty = 1, pch = 2, col = 2, type = 'o')
+
+legend( "topleft", leg = c("txqueuelen 37", "txqueuelen 1000"), lty = 1, pch = 1:2, col = 1:2)
+
+title("Bufferbloat reduced at a CeroWrt Access Point\n6 stations doing simultaneous TCP transfers")
+
+dev.off()
