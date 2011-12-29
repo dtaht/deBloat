a361d82cc8b279671e2d130e08875499af1c9058
Dave Taht <d@taht.net>
2011-12-29 10:43:45 +0100

    Lua wrapper for iperf modulularized

--- test/iperf.lua
+++ test/iperf.lua
@@ -1,42 +1,20 @@
 #!/usr/bin/lua
 
+-- module(...,
+require "cero"
+require "csv"
+
 ip="172.30.49.27"
 tests="iperf -yc -t %d -w256k -c %s"
 
 numtests=10
 tmon = { }
 
-function fromCSV (s)
-   s = s .. ','        -- ending comma
-   local t = {}        -- table to collect fields
-   local fieldstart = 1
-   repeat
-      -- next field is quoted? (start with `"'?)
-      if string.find(s, '^"', fieldstart) then
-	 local a, c
-	 local i  = fieldstart
-	 repeat
-            -- find closing quote
-            a, i, c = string.find(s, '"("?)', i+1)
-	 until c ~= '"'    -- quote not followed by quote?
-	 if not i then error('unmatched "') end
-	 local f = string.sub(s, fieldstart+1, i-1)
-	 table.insert(t, (string.gsub(f, '""', '"')))
-	 fieldstart = string.find(s, ',', i) + 1
-      else                -- unquoted; find next comma
-	 local nexti = string.find(s, ',', fieldstart)
-	 table.insert(t, string.sub(s, fieldstart, nexti-1))
-	 fieldstart = nexti + 1
-      end
-   until fieldstart > string.len(s)
-   return t
-end
-
 totalbytes=0
 bytespersec=0
 
 local function iperfprint(s) 
-   local t = fromCSV(s)
+   local t = csv:fromCSV(s)
    for i,v in ipairs(t) do print(i,v) end
    totalbytes= totalbytes + t[8]
    bytespersec= bytespersec + t[9]
@@ -44,7 +22,7 @@ end
 
 
 local function runtest(args)
-return io.popen(args,"r")
+   return io.popen(args,"r")
 end
 
 t = string.format(tests,10,ip)
