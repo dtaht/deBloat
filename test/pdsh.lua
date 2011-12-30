23c2619b23c4c92e2bdf1ff6a77acf78c576e4c4
Dave Taht <d@taht.net>
2011-12-30 22:51:34 +0100

    Want to use pdsh to get information from various clients.
    So started at a wrapper for it.

---
+++ test/pdsh.lua
@@ -0,0 +1,19 @@
+-- pdsh based controller for various clients
+
+module (...,package.seeall)
+
+function pdsh(s)
+end
+
+function reboot(s) 
+end
+function reset(s)
+end
+function change_qos(s)
+end
+function get_stats(s)
+end
+function uptime(s)
+end
+function loadavg(s)
+end
