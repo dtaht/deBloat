5245bf0eac2ed121b0a64563c7b5b091e916f5f3
Dave Taht <d@taht.net>
2012-01-10 10:47:02 +0100

    New, improved 'debloat' script that does more stuff, more cleanly
    Still is showing it's heritage as a shell script...
    
    does a few more qdiscs now, not necessarily correctly, I
    have to think about how to manage the depths right in this arch.

--- src/qmodels/sfq.lua
+++ src/qmodels/sfq.lua
@@ -1,4 +1,13 @@
 -- simple SFQ model
+-- usage:
+
+-- sfq = model:sfq.new(env)
+-- mq = model:mq.new(env)
+-- mq.VO{BINS=12}
+-- mq.VI{BINS=10}
+-- mq.BE{BINS=1000}
+-- sfq:attach(mq)
+-- mq.generate()
 
 module(...,package.seeall)
 
